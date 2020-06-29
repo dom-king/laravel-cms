7235400af8475f5a2b1dfcb2ac302a5e56f9ddb4
Dom King <dminicking91@gmail.com>
2020-06-29 12:16:36 +0100

    updated the post controller to pass through data doing basic laravel fundamentals

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Contact Page
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/about', function () {
-    return 'Hi About Page';
-});
+// Route::get('/about', function () {
+//     return 'Hi About Page';
+// });
 
-Route::get('/contact', function () {
-    return 'Hi Contact Page';
-}); 
+// Route::get('/contact', function () {
+//     return 'Hi Contact Page';
+// }); 
 
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return 'This is post number ' . $id . ' ' .$name;
-});
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return 'This is post number ' . $id . ' ' .$name;
+// });
 
-Route::get('admin/posts/example', array('as' => 'admin.home', function () {
-    $url = route('admin.home');
-    return 'this url is ' . $url;
-}));
+// Route::get('admin/posts/example', array('as' => 'admin.home', function () {
+//     $url = route('admin.home');
+//     return 'this url is ' . $url;
+// }));
+
+// Route::resource('posts', 'PostsController');
+Route::get('/contact', 'PostsController@contact');
+Route::get('/post/{id}/{name}', 'PostsController@show_post');
