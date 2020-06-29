bb57ec564a9f19de8b0cc7f00b141186f3715e5f
Dom King <dminicking91@gmail.com>
2020-06-29 10:15:46 +0100

    created route examples following edwins laravel route fundamentals

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'Hi About Page';
+});
+
+Route::get('/contact', function () {
+    return 'Hi Contact Page';
+}); 
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return 'This is post number ' . $id . ' ' .$name;
+});
+
+Route::get('admin/posts/example', array('as' => 'admin.home', function () {
+    $url = route('admin.home');
+    return 'this url is ' . $url;
+}));
